fc558cb11146d50a1614f72bb42b81e5615c7a9e
Aaron Stubblefield <ags2189@columbia.edu>
2021-11-18 12:12:30 -0500

    removed command line parameter options for now

--- source/params.py
+++ source/params.py
@@ -1,38 +1,11 @@
 # All model parameters and options are set here.
 
-import sys,argparse
-
-parser = argparse.ArgumentParser(\
-    description='Set parameters for subglacial lake problem.')
-parser.add_argument('-C', type=float, default=5.0e9, metavar='drag_coeff',
-                    help='Basal drag coefficient (Pa s/m)')
-parser.add_argument('-H', type=float, default=1000.0, metavar='thickness',
-                    help='Ice thickness (m)')
-parser.add_argument('-L', type=float, default=80.0, metavar='domain length',
-                    help='Length of domain (km) in x and y directions')
-parser.add_argument('-pd', type=float, default=5, metavar='period',
-                    help='Filling/draining oscillation period (yr)')
-parser.add_argument('-plotting', type=str, default='off', metavar='on/off',
-                    help='Turn real-time plotting \'on\' or \'off\' ')
-parser.add_argument('-print_info', type=str, default='on', metavar='on/off',
-                    help='Turn \'on\' to print Newton convergence info ')
-parser.add_argument('-save_vtk', type=str, default='off', metavar='on/off',
-                    help='Turn \'on\' to save Stokes vtk files ')
-args, unknown = parser.parse_known_args()
-
 import numpy as np
 #-------------------------------------------------------------------------------
 #-----------------------------MODEL OPTIONS-------------------------------------
 
-# Turn 'on' or 'off' real-time plotting that saves a png figure called 'surfs' at
-# each time step of the free surface geometry.
-realtime_plot = args.plotting
-
-# Turn 'on' or 'off' Newton convergence information:
-print_convergence = args.print_info
-
 # save vtk files for stokes solution if 'on':
-save_vtk = args.save_vtk
+save_vtk = 'off'
 
 
 # real-time plotting
@@ -55,7 +28,7 @@ B = (2**((n-1.0)/(2*n)))*B0        # coefficient in weak form (Pa s^{1/n})
 rho_i = 917.0                      # density of ice (kg/m^3)
 rho_w = 1000.0                     # density of water (kg/m^3)
 g = 9.81                           # gravitational acceleration (m/s^2)
-C = args.C                         # sliding law friction coefficient (Pa s/m)
+C = 5.0e9                          # sliding law friction coefficient (Pa s/m)
 
 # numerical parameters
 eps_p = 1.0e-13                    # penalty method parameter for unilateral condition
@@ -73,11 +46,11 @@ tol = 1.0e-1                       # numerical tolerance for boundary geometry:
                                    # s(x,t) - b(x) <= tol on ice-bed boundary.
 
 # geometry/mesh parameters
-Hght = args.H                      # (initial) height of the domain (m)
-Lngth = args.L*1000.0              # length of the domain (m)
+Hght = 1000.0                      # (initial) height of the domain (m)
+Lngth = 80*1000.0                  # length of the domain (m)
 
-Ny = int(Lngth/2500.0)               # number of elements in vertical direction
-Nx = int(Lngth/2500.0)              # number of elements in horizontal direction
+Ny = int(Lngth/2500.0)             # number of elements in vertical direction
+Nx = int(Lngth/2500.0)             # number of elements in horizontal direction
 Nz = int(Hght/500.0)
 
 DX = Lngth/Nx
@@ -85,24 +58,21 @@ DY = Lngth/Ny
 DZ = Hght/Nz
 
 # time-stepping parameters
-t_period = args.pd*3.154e7         # oscillation period (secs; yr*sec_per_year)
-t_final = 2*t_period            # final time
-nt_per_cycle = 2000                 # number of timesteps per oscillation
-nt = int(t_final/t_period*nt_per_cycle) # number of time steps
-dt = t_final/nt                    # timestep size
-t_arr = np.linspace(0,t_final,nt)
+t_period = 5*3.154e7                     # oscillation period (sec)
+t_final = 2*t_period                     # final time
+nt_per_cycle = 2000                      # number of timesteps per oscillation
+nt = int(t_final/t_period*nt_per_cycle)  # number of time steps
+dt = t_final/nt                          # timestep size
+t_arr = np.linspace(0,t_final,nt)        # time array (mainly for plotting)
 
-# spatial coordinate for plotting and interpolation
-
-nx = 100*Nx                          # number of grid points for interpolating
-                                   # free surfaces and plotting (larger
-                                   # than true number elements Nx)
+# spatial coordinates for plotting and interpolation on fine grid
+nx = 100*Nx                        # number of grid points for plotting
+                                   # (larger than true number of elements Nx)
 ny = nx
 
 
-X_fine = np.linspace(0,Lngth,nx)   # horizontal coordinate for computing surface
-                                   # slopes and plotting.
+X_fine = np.linspace(0,Lngth,nx)   # horizontal x coordinate
 
-Y_fine = np.linspace(0,Lngth,nx)   # horizontal coordinate for computing surface
+Y_fine = np.linspace(0,Lngth,ny)   # horizontal y coordinate
 
 X0,Y0 = np.meshgrid(X_fine,Y_fine,indexing='ij')
